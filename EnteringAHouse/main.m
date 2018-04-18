93d1ee909af69accd10c29aa4e54bf2de9c26e3d
Mike Cameron <mike@mikecameron.tv>
2018-04-18 15:38:44 -0400

    Initial Commit

---
+++ EnteringAHouse/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EnteringAHouse
+//
+//  Created by Mike Cameron on 2018-04-18.
+//  Copyright © 2018 Mike Cameron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
