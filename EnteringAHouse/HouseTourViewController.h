726df8d1917f5ebae6147acda53b35391299a3ed
Mike Cameron <mike@mikecameron.tv>
2018-04-18 16:25:11 -0400

    Layout done in view controllers

---
+++ EnteringAHouse/HouseTourViewController.h
@@ -0,0 +1,13 @@
+//
+//  HouseTourViewController.h
+//  EnteringAHouse
+//
+//  Created by Mike Cameron on 2018-04-18.
+//  Copyright © 2018 Mike Cameron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HouseTourViewController : UIViewController
+
+@end
---
+++ EnteringAHouse/HouseTourViewController.m
@@ -0,0 +1,37 @@
+//
+//  HouseTourViewController.m
+//  EnteringAHouse
+//
+//  Created by Mike Cameron on 2018-04-18.
+//  Copyright © 2018 Mike Cameron. All rights reserved.
+//
+
+#import "HouseTourViewController.h"
+
+@interface HouseTourViewController ()
+
+@end
+
+@implementation HouseTourViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
